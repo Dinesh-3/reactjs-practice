fbcf7a1d5e2b020d2fb9ddef835eefbbbe91394e
Dinesh Iyyandurai <dinesh.i@godigit.com>
2022-04-28 10:08:07 +0530

    Guess My Secret

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import GuessSecretNumber from "./guessSecretNumber";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <nav>
+                                <Link to="/">Home</Link> | <Link to="/guess-secret-number">Guess Secret Number</Link>
+                            </nav>
+                        }
+                    />
+                    <Route path="/guess-secret-number" element={<GuessSecretNumber />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/guessSecretNumber/index.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import "./style.css";
+
+const MAX_RANGE = 10;
+
+const RESULT = {
+    LOW: "Too Low",
+    HIGH: "Too High",
+    CORRECT: "Correct Answer!",
+    GAME_OVER: "Game Over! Try Again",
+    GUESS: "Start Guessing"
+};
+
+const initialContext = {
+    score: MAX_RANGE,
+    highScore: 0,
+    answer: Number.parseInt(Math.random() * MAX_RANGE),
+    result: RESULT.GUESS,
+    showAnswer: "?"
+};
+
+const GuessSecretNumber = () => {
+    const [context, setContext] = useState(initialContext);
+
+    const [userGuess, setUserGuess] = useState("");
+
+    const checkAnswer = () => {
+        if (!userGuess) return;
+
+        const prevContext = { ...context };
+
+        prevContext.showAnswer = "?";
+
+        if (context.score === 0) {
+            prevContext.result = RESULT.GAME_OVER;
+            return setContext(prevContext);
+        }
+
+        if (userGuess === context.answer) {
+            prevContext.score += 1;
+            prevContext.result = RESULT.CORRECT;
+
+            prevContext.showAnswer = prevContext.answer;
+            prevContext.highScore = Math.max(prevContext.score, prevContext.highScore);
+
+            prevContext.answer = Number.parseInt(Math.random() * 10);
+        } else {
+            prevContext.score -= 1;
+            prevContext.result = userGuess < context.answer ? RESULT.LOW : RESULT.HIGH;
+        }
+
+        // setUserGuess("");
+        setContext(prevContext);
+    };
+
+    const handleUserGuessChange = (e) => {
+        const number = Number.parseInt(e.target.value);
+        if (number === NaN) setUserGuess(null);
+        setUserGuess(number);
+    };
+
+    const handleTryAgain = () => {
+        setContext(initialContext);
+        setUserGuess("");
+    };
+
+    return (
+        <React.Fragment>
+            <header>
+                <h1>Guess My Number!</h1>
+                <p className="between">(Between 1 and {MAX_RANGE})</p>
+                <button className="btn again" onClick={handleTryAgain}>
+                    Again!
+                </button>
+                <div className="number">{context.showAnswer}</div>
+            </header>
+            <main className={`${context.showAnswer !== "?" ? "bg-green" : ""}`}>
+                <section className="left">
+                    <input type="number" className="guess" value={userGuess} onChange={handleUserGuessChange} />
+                    <button className="btn check" onClick={checkAnswer}>
+                        Check!
+                    </button>
+                </section>
+                <section className="right">
+                    <p className="message">{context.result}</p>
+                    <p className="label-score">
+                        💯 Score: <span className="score">{context.score}</span>
+                    </p>
+                    <p className="label-highscore">
+                        🥇 Highscore: <span className="highscore">{context.highScore}</span>
+                    </p>
+                </section>
+            </main>
+        </React.Fragment>
+    );
+};
+
+export default GuessSecretNumber;
